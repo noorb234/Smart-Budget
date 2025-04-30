e66144d1f181f48a0f42a050c8c3d0cd5ea2b0cf
KenKohl <Kohlka@farmingdale.edu>
2025-04-30 15:04:52 -0400

    Worked on slider to integrate the changes Noor posted with visually switching the theme with the database storage of the theme written by Ken. Also fixed authentication page to match page theme.

--- PHPFiles/toggleTheme.js
+++ PHPFiles/toggleTheme.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeSwitch = document.getElementById('theme-switch');
     
     // Apply the initial theme based on user preference
-    const currentTheme = document.documentElement.getAttribute('data-theme') === 'dark-mode' ? 'dark mode' : 'light mode';
+    //const currentTheme = document.documentElement.getAttribute('data-theme') === 'dark-mode' ? 'dark mode' : 'light mode';
+	const currentTheme = document.body.classList.contains('dark-mode') ? 'dark mode' : 'light mode';
     themeSwitch.checked = currentTheme === 'dark mode';
 
     // Toggle theme when the user interacts with the switch
